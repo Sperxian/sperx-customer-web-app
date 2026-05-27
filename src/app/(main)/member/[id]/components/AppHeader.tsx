0c5012219b9befd58792b126559f065b3417a863
Pool Golez <loop.edward@gmail.com>
2026-05-27 19:42:06 +0800

    SPX-6: Add user profile on app header

--- src/app/(main)/member/[id]/components/AppHeader.tsx
+++ src/app/(main)/member/[id]/components/AppHeader.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useMemberLoyalty } from "@/app/(main)/member/[id]/MemberContext";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export function AppHeader() {
@@ -12,24 +13,27 @@ export function AppHeader() {
   } = useMemberLoyalty();
 
   return (
-    <header className="flex items-center gap-3 bg-primary px-4 py-3 flex-shrink-0">
-      <Image
-        className="aspect-square bg-white rounded-full object-scale-down"
-        src={iconLocation}
-        alt={shopName}
-        width={36}
-        height={36}
-        priority
-      />
-      <div>
-        <h1 className="text-white text-xl font-medium leading-tight">
-          {shopName}
-        </h1>
+    <header className="flex items-center justify-between gap-3 bg-primary px-4 py-3 flex-shrink-0">
+      <div className="flex items-center gap-3 flex-shrink-0">
+        <Image
+          className="aspect-square bg-white rounded-full object-scale-down"
+          src={iconLocation}
+          alt={shopName}
+          width={36}
+          height={36}
+          priority
+        />
+        <div>
+          <h1 className="text-white text-xl font-medium leading-tight">
+            {shopName}
+          </h1>
 
-        <p className="text-white/50 text-xs leading-tight">
-          {loyaltyProgramName}
-        </p>
+          <p className="text-white/50 text-xs leading-tight">
+            {loyaltyProgramName}
+          </p>
+        </div>
       </div>
+      <UserButton />
     </header>
   );
 }
