82c5041dbcc8f4d20f286fe650924104d885ebbc
Pool Golez <loop.edward@gmail.com>
2026-05-27 11:18:16 +0800

    SPX-6: Add reusable alert

---
+++ src/app/components/shared/Alert.tsx
@@ -0,0 +1,67 @@
+import {
+  TriangleAlertIcon,
+  CircleXIcon,
+  CheckCircleIcon,
+  InfoIcon,
+} from "lucide-react";
+
+type Variant = "warning" | "error" | "success" | "info";
+
+const variantConfig = {
+  warning: {
+    container: "border-amber-600 text-amber-600",
+    iconBg: "bg-amber-600",
+    Icon: TriangleAlertIcon,
+  },
+  error: {
+    container: "border-red-600 text-red-600",
+    iconBg: "bg-red-600",
+    Icon: CircleXIcon,
+  },
+  success: {
+    container: "border-green-600 text-green-600",
+    iconBg: "bg-green-600",
+    Icon: CheckCircleIcon,
+  },
+  info: {
+    container: "border-blue-600 text-blue-600",
+    iconBg: "bg-blue-600",
+    Icon: InfoIcon,
+  },
+};
+
+type AlertProps = {
+  message: string;
+  variant?: Variant;
+  className?: string;
+  actionSlot?: React.ReactNode;
+};
+
+export function Alert({
+  message,
+  variant = "warning",
+  className = "",
+  actionSlot = undefined,
+}: AlertProps) {
+  const { container, iconBg, Icon } = variantConfig[variant];
+
+  return (
+    <div
+      className={[
+        "border border-2 rounded-2xl p-2 flex items-start gap-2 text-sm font-medium",
+        container,
+        className,
+      ].join(" ")}
+    >
+      <div className={`aspect-square p-1.5 rounded-lg ${iconBg}`}>
+        <Icon className="text-white" />
+      </div>
+      <div className="flex flex-col">
+        <span className="text-md font-bold">Keep your loyalty progress safe</span>
+        <span className="text-xs font-medium">{message}</span>
+
+        {actionSlot && <div className="mt-2">{actionSlot}</div>}
+      </div>
+    </div>
+  );
+}
