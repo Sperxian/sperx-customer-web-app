3f6691dd72870432671bf59daf853eef4d104e99
Pool Golez <loop.edward@gmail.com>
2026-05-27 11:32:00 +0800

    SPX-6: Add sign up reminder

--- src/app/(main)/member/[id]/page.tsx
+++ src/app/(main)/member/[id]/page.tsx
@@ -1,19 +1,45 @@
+"use client";
+
 import { ActivityHistorySection } from "@/app/(main)/member/[id]/components/ActivityHistorySection";
 import { HowItWorksSection } from "@/app/(main)/member/[id]/components/HowItWorksSection";
 import { LoyaltyCardSection } from "@/app/(main)/member/[id]/components/LoyaltyCardSection";
+import { Alert } from "@/app/components/shared/Alert";
 import { getMemberPointsHistory } from "@/lib/api/member";
+import { useMemberLoyalty } from "./MemberContext";
+import { useEffect, useState } from "react";
+import { MemberPointsHistory } from "@/types/domain";
 
-interface PageParams {
-  params: Promise<{
-    id: string;
-  }>;
-}
-export default async function MemberPage({ params }: PageParams) {
-  const { id: memberId } = await params;
-  const history = await getMemberPointsHistory(memberId);
+export default function MemberPage() {
+  const memberLoyalty = useMemberLoyalty();
+  const [history, setHistory] = useState<MemberPointsHistory | null>();
+
+  useEffect(() => {
+    async function fetchHistory() {
+      const data = await getMemberPointsHistory(memberLoyalty.id);
+      setHistory(data);
+    }
+
+    fetchHistory();
+  }, [memberLoyalty]);
+
+  const showSignUpReminder = memberLoyalty.points >= 2;
+  const signUpReminder = (
+    <Alert
+      variant="warning"
+      message="Guest accounts are stored only on this device. Secure your stamps with an account."
+      actionSlot={
+        <div className="flex gap-4 justify-end">
+          <button className="text-primary font-bold underline mt-1 capitalize">
+            Sign up
+          </button>
+        </div>
+      }
+    />
+  );
 
   return (
-    <div className="grid gap-8 p-4">
+    <div className="grid gap-4 p-4">
+      {showSignUpReminder && signUpReminder}
       <LoyaltyCardSection />
       {history && history.items.length === 0 && <HowItWorksSection />}
       {history && <ActivityHistorySection history={history} />}
