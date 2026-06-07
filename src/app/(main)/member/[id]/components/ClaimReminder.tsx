3b26fa82df4c136fb5a9e0699171d76b2627a8c7
Pool Golez <loop.edward@gmail.com>
2026-06-07 18:28:25 +0800

    SPX-6: Extract claim reminder alert to allow  other scenarios

---
+++ src/app/(main)/member/[id]/components/ClaimReminder.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Alert } from "@/app/components/shared/Alert";
+import { MemberLoyalty } from "@/types/domain";
+import { SignUpButton, useUser } from "@clerk/nextjs";
+
+type Props = {
+  memberLoyalty: MemberLoyalty;
+};
+
+export default function ClaimReminder({ memberLoyalty }: Props) {
+  const { isLoaded, isSignedIn } = useUser();
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  const showSignUpReminder = isSignedIn === false && memberLoyalty.points >= 2;
+
+  let metadata;
+  // TODO: If signed in, but member loyalty has not yet been claimed!
+  if (showSignUpReminder) {
+    metadata = {
+      message:
+        "Guest accounts are stored only on this device. " +
+        "Secure your stamps with an account.",
+      actionText: "Sign up",
+    };
+  }
+
+  if (!metadata) {
+    return null;
+  }
+
+  return (
+    <Alert
+      variant="warning"
+      message={metadata.message}
+      actionSlot={
+        <div className="flex gap-4 justify-end">
+          <SignUpButton
+            mode="modal"
+            forceRedirectUrl={`/member/${memberLoyalty.id}/claim`}
+          >
+            <button className="text-primary font-bold underline mt-1 capitalize">
+              {metadata.actionText}
+            </button>
+          </SignUpButton>
+        </div>
+      }
+    />
+  );
+}
--- src/app/(main)/member/[id]/page.tsx
+++ src/app/(main)/member/[id]/page.tsx
@@ -3,17 +3,15 @@
 import { ActivityHistorySection } from "@/app/(main)/member/[id]/components/ActivityHistorySection";
 import { HowItWorksSection } from "@/app/(main)/member/[id]/components/HowItWorksSection";
 import { LoyaltyCardSection } from "@/app/(main)/member/[id]/components/LoyaltyCardSection";
-import { Alert } from "@/app/components/shared/Alert";
 import { getMemberPointsHistory } from "@/lib/api/member";
 import { useMemberLoyalty } from "./MemberContext";
 import { useEffect, useState } from "react";
 import { MemberPointsHistory } from "@/types/domain";
-import { SignUpButton, useUser } from "@clerk/nextjs";
 import { themeCssVars } from "@/lib/theme";
+import ClaimReminder from "./components/ClaimReminder";
 
 export default function MemberPage() {
   const memberLoyalty = useMemberLoyalty();
-  const { isSignedIn } = useUser();
   const [history, setHistory] = useState<MemberPointsHistory | null>();
 
   const { theme } = memberLoyalty.shop.config;
@@ -28,32 +26,11 @@ export default function MemberPage() {
     fetchHistory();
   }, [memberLoyalty]);
 
-  const showSignUpReminder = isSignedIn === false && memberLoyalty.points >= 2;
-
-  const signUpReminder = (
-    <Alert
-      variant="warning"
-      message="Guest accounts are stored only on this device. Secure your stamps with an account."
-      actionSlot={
-        <div className="flex gap-4 justify-end">
-          <SignUpButton
-            mode="modal"
-            forceRedirectUrl={`/member/${memberLoyalty.id}/link`}
-          >
-            <button className="text-primary font-bold underline mt-1 capitalize">
-              Sign up
-            </button>
-          </SignUpButton>
-        </div>
-      }
-    />
-  );
-
   return (
     <div className="flex-1 relative overflow-hidden" style={themeVars}>
       <main className="h-full overflow-y-auto">
         <div className="grid gap-4 p-4">
-          {showSignUpReminder && signUpReminder}
+          <ClaimReminder memberLoyalty={memberLoyalty} />
           <LoyaltyCardSection />
           {history && history.items.length === 0 && <HowItWorksSection />}
           {history && <ActivityHistorySection history={history} />}
