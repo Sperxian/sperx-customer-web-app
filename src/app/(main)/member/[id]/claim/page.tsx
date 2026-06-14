f78ed5d80dd65b979a35a63df897bbcff2d2c24a
Pool Golez <loop.edward@gmail.com>
2026-06-14 09:22:56 +0800

    SPX-6: Handle linking of claim

--- src/app/(main)/member/[id]/claim/page.tsx
+++ src/app/(main)/member/[id]/claim/page.tsx
@@ -2,11 +2,18 @@
 
 import { useUser } from "@clerk/nextjs";
 import { LoyaltyCardSection } from "../components/LoyaltyCardSection";
-import { LinkIcon } from "lucide-react";
-import { notFound } from "next/navigation";
+import { LinkIcon, LoaderCircleIcon } from "lucide-react";
+import { notFound, useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { Alert } from "@/app/components/shared/Alert";
 
 export default function ClaimMemberLoyaltyPage() {
-  const { isLoaded: isUserLoaded, isSignedIn,  user } = useUser();
+  const { id: memberId } = useParams();
+  const router = useRouter();
+
+  const { isLoaded: isUserLoaded, isSignedIn, user } = useUser();
+  const [isLinking, setIsLinking] = useState(false);
+  const [linkError, setLinkError] = useState<string>();
 
   if (isUserLoaded && !isSignedIn) {
     return notFound();
@@ -14,11 +21,53 @@ export default function ClaimMemberLoyaltyPage() {
 
   const email = user?.primaryEmailAddress?.toString();
 
+  const linkMember = async () => {
+    setIsLinking(true);
+    try {
+      const response = await fetch(`/api/members/${memberId}/claim`, {
+        method: "post",
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        router.replace(`/member/${memberId}`);
+        return;
+      }
+
+      setLinkError(data.message || "An unexpected error has occured.");
+    } finally {
+      setIsLinking(false);
+    }
+  };
+
+  const goBack = () => {
+    router.back();
+  };
+
   return (
     <div className="h-full flex flex-col gap-4 p-4">
       <div className="text-xl font-bold capitalize">
         Claim this loyalty card
       </div>
+
+      {linkError && (
+        <Alert
+          variant="error"
+          title="Error While Claiming Member"
+          message={linkError}
+          actionSlot={
+            <div className="w-full flex justify-end">
+              <button
+                className="text-primary dark:text-primary-lighter font-bold underline mt-1 capitalize"
+                onClick={() => goBack()}
+              >
+                Go back
+              </button>
+            </div>
+          }
+        />
+      )}
 
       <div className="text-sm text-foreground">
         We found this loyalty card stored on this device. Link it to your
@@ -30,16 +79,23 @@ export default function ClaimMemberLoyaltyPage() {
       {isUserLoaded && (
         <div className="flex flex-col gap-2">
           <button
-            className="bg-primary hover:bg-primary/80 text-white p-4 rounded-lg w-full inline-flex items-center justify-center gap-2"
-            onClick={() => {}}
+            className="bg-primary disabled:bg-primary/80 text-white p-4 rounded-lg w-full inline-flex items-center justify-center gap-2"
+            disabled={isLinking}
+            onClick={linkMember}
           >
-            <LinkIcon size={18} />
-            Claim it under {email}
+            {!isLinking ? (
+              <LinkIcon size={18} />
+            ) : (
+              <LoaderCircleIcon
+                size={18}
+                className="animate-spin aspect-square text-secondary/80 rounded-xl"
+              />
+            )}
+            <div className="text-md uppercase font-medium tracking-wide">
+              Claim This Member
+            </div>
           </button>
-          {/* <div className="mt-4 flex flex-col text-sm text-foreground/80">
-          <span>Skip claiming this loyalty card?</span>
-          <span>Logout</span>
-        </div> */}
+          <div className="text-xs w-full text-center">({email})</div>
         </div>
       )}
     </div>
