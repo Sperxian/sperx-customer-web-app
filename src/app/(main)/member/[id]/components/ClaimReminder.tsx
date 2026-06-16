1e2ab896a81c0d5a015ddb2ba8a4a96b10256e0d
Pool Golez <loop.edward@gmail.com>
2026-06-16 18:48:16 +0800

    SPX-6: Add message for claiming loyalty (signed in)

--- src/app/(main)/member/[id]/components/ClaimReminder.tsx
+++ src/app/(main)/member/[id]/components/ClaimReminder.tsx
@@ -3,6 +3,7 @@
 import { Alert } from "@/app/components/shared/Alert";
 import { MemberLoyalty } from "@/types/domain";
 import { SignUpButton, useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
 type Props = {
   memberLoyalty: MemberLoyalty;
@@ -10,21 +11,55 @@ type Props = {
 
 export default function ClaimReminder({ memberLoyalty }: Props) {
   const { isLoaded, isSignedIn } = useUser();
+  const router = useRouter();
+
+  const goToClaimPage = () => {
+    router.push(`/member/${memberLoyalty.id}/claim`);
+  };
 
   if (!isLoaded) {
     return null;
   }
 
   const showSignUpReminder = isSignedIn === false && memberLoyalty.points >= 2;
+  const showClaimReminder = isSignedIn === true && !memberLoyalty.isClaimed;
 
   let metadata;
-  // TODO: If signed in, but member loyalty has not yet been claimed!
   if (showSignUpReminder) {
     metadata = {
+      title: "Keep your loyalty progress safe",
       message:
         "Guest accounts are stored only on this device. " +
         "Secure your stamps with an account.",
       actionText: "Sign up",
+      action: (
+        <div className="flex gap-4 justify-end">
+          <SignUpButton
+            mode="modal"
+            forceRedirectUrl={`/member/${memberLoyalty.id}/claim`}
+          >
+            <button className="text-primary dark:text-primary-lighter font-bold underline mt-1 capitalize">
+              Sign up
+            </button>
+          </SignUpButton>
+        </div>
+      ),
+    };
+  } else if (showClaimReminder) {
+    metadata = {
+      title: "Is this your loyalty card?",
+      message: "Secure this loyalty card by linking it to your account.",
+      actionText: "Claim this",
+      action: (
+        <div className="flex gap-4 justify-end">
+          <button
+            className="text-primary dark:text-primary-lighter font-bold underline mt-1 capitalize"
+            onClick={goToClaimPage}
+          >
+            Claim Now
+          </button>
+        </div>
+      ),
     };
   }
 
@@ -35,20 +70,9 @@ export default function ClaimReminder({ memberLoyalty }: Props) {
   return (
     <Alert
       variant="warning"
-      title="Keep your loyalty progress safe"
+      title={metadata.title}
       message={metadata.message}
-      actionSlot={
-        <div className="flex gap-4 justify-end">
-          <SignUpButton
-            mode="modal"
-            forceRedirectUrl={`/member/${memberLoyalty.id}/claim`}
-          >
-            <button className="text-primary dark:text-primary-lighter font-bold underline mt-1 capitalize">
-              {metadata.actionText}
-            </button>
-          </SignUpButton>
-        </div>
-      }
+      actionSlot={metadata.action}
     />
   );
 }
