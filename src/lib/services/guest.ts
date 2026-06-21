3f892b967d279973ea94e4c1facee55e8870b572
Pool Golez <loop.edward@gmail.com>
2026-06-21 17:57:46 +0800

    SPX-23: Add service method to fetch member loyalties store in localStorage

--- src/lib/services/guest.ts
+++ src/lib/services/guest.ts
@@ -1,2 +1,35 @@
+import { getMemberLoyalty } from "../api/member";
+
 export const SPX_GUEST_COOKIE_NAME = 'spx-guest';
 export const SPX_GUEST_HEADER_NAME = 'spx-guest';
+
+type MemberData = {
+  memberId: string;
+  dateCreated: Date;
+};
+
+export async function fetchGuestMemberLoyalties() {
+  const items: Record<string, MemberData> = retrieveLocalStorageData();
+  const memberIds = Object.values(items)
+    .map(({ memberId }) => memberId);
+
+  const memberLoyalties = await Promise.all(memberIds.map((id) => getMemberLoyalty(id)));
+
+  return memberLoyalties.filter((loyalty) => !!loyalty);
+}
+
+function retrieveLocalStorageData() {
+  const items: Record<string, MemberData> = {};
+
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    if (key && key.startsWith('spx-loyalty-')) {
+      const value = localStorage.getItem(key)
+      if (value) {
+        items[key] = JSON.parse(value) as MemberData;
+      }
+    }
+  }
+
+  return items;
+}
