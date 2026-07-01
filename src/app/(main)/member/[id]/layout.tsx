bee03bb82236ef5cebc451445344ee1eca881d63
Pool Golez <loop.edward@gmail.com>
2026-07-01 18:42:08 +0800

    SPX-25: Add forbidden page when API endpoint returns 403

--- src/app/(main)/member/[id]/layout.tsx
+++ src/app/(main)/member/[id]/layout.tsx
@@ -2,6 +2,8 @@ import { AppHeader } from "@/app/(main)/member/[id]/components/AppHeader";
 import { getMemberLoyalty } from "@/lib/api/member";
 import { MemberLoyaltyContextProvider } from "./MemberContext";
 import { notFound } from "next/navigation";
+import { isAxiosError } from "axios";
+import ForbiddenContent from "@/app/(main)/member/[id]/components/ForbiddenContent";
 import "@/app/globals.css";
 
 export default async function MemberLayout({
@@ -14,7 +16,15 @@ export default async function MemberLayout({
   }>;
 }>) {
   const { id: memberId } = await params;
-  const memberLoyalty = await getMemberLoyalty(memberId);
+  let memberLoyalty = null;
+  try {
+    memberLoyalty = await getMemberLoyalty(memberId);
+  } catch (error) {
+    if (isAxiosError(error) && error.response?.status === 403) {
+      return <ForbiddenContent />;
+    }
+  }
+
   if (!memberLoyalty) {
     return notFound();
   }
